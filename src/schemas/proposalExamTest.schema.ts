ba4c3f5cd5eb7e184dc99b9ab3f52d6bae7a30c3
EPN-02 <emanuelpuscasu16@gmail.com>
2024-12-10 19:09:18 +0200

    fix: added validation on date input

--- src/schemas/proposalExamTest.schema.ts
+++ src/schemas/proposalExamTest.schema.ts
@@ -16,17 +16,19 @@ export const examProposalSchema = z.object({
     .string()
     .min(1, "The title field is required!")
     .max(40, "The title shouldn't be longer than 40 characters!"),
-    studyType: z.string().refine(
-      (val) => {
-        if (val === "licenta" || val === "master") {
-          return true;
-        }
-        return false;
-      },
-      {
-        message: "The study type field is required!",
-      },
-    ),
-    group: z.string().min(1, "The group field is required!"),
-    description: z.string(),
+  studyType: z.string().refine(
+    (val) => {
+      if (val === "licenta" || val === "master") {
+        return true;
+      }
+      return false;
+    },
+    {
+      message: "The study type field is required!",
+    },
+  ),
+  date: z.string().date("The date is invalid!"),
+
+  group: z.string().min(1, "The group field is required!"),
+  description: z.string(),
 });
